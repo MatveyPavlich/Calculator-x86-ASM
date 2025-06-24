3ff4b99261ca99b4de6dbca441649326ce50db54
Matvey Pavlich <matveypavlich@gmail.com>
2025-06-24 21:56:56 +0100

    feat: add support for brackets; store signes for each input value

--- src/main.asm
+++ src/main.asm
@@ -9,7 +9,9 @@
 %define num2         memory_buffer + 3
 %define op           memory_buffer + 6
 %define result       memory_buffer + 9
-%define equation     memory_buffer + 12
+%define equation     memory_buffer + 50
+%define sign1        memory_buffer + 20
+%define sign2        memory_buffer + 21
     
 %include "./src/data.asm"
 %include "./src/functions.asm"
@@ -18,18 +20,20 @@ section .text
 global _start
 
 _start:
+    MOV esi, equation                    ; Store pointer to the equation
+    
     ; Get all values
     print welcome_msg, welcome_msg_len   ; Print welcome message
     print ask_input_1, ask_input_1_len   ; Ask user for the first operand
     read num1, 10                        ; Read user input using a syscall
-    input_check num1                     ; Check the input is valid
+    input_check num1, sign1              ; Check the input is valid
     print show_oprtns, show_oprtns_len   ; Display available operations
     print ask_oprtn, ask_oprtn_len       ; Ask user for the operation
     read op, 2                           ; Read user input using a syscall
-    input_check op                       ; Check the input is valid
+    input_check op, (op + 1)             ; Check the input is valid (don't need a sign here)
     print ask_input_2, ask_input_2_len   ; Ask user for the second operand
     read num2, 10                        ; Read user input using a syscall
-    input_check num2                     ; Check the input is valid
+    input_check num2, sign1              ; Check the input is valid
 
     ; ASCII -> INT conversion
     MOV cl, [op]                         ; Move the opperation code into cl
